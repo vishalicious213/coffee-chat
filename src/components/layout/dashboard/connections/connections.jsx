1c0f680f951e1be69e71fe235a2833d1a70c7e46
Nabeeha <nabeehaghani@gmail.com>
2025-02-17 17:05:27 -0600

    dashboard desktop version

--- src/components/layout/dashboard/connections/connections.jsx
+++ src/components/layout/dashboard/connections/connections.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ConnectionsCard from './connections-card'
 import "./connections.css"
 
-const test = ["Vish Singh", "Samir Shuman", "Nabeeha Ghani", "Giovanni Nazario"]
+const test = ["Vish Singh", "Samir Shuman", "Nabeeha Ghani", "Giovanni Nazario", "Vish Singh", "Samir Shuman", "Nabeeha Ghani", "Vish Singh", "Samir Shuman"]
 
 const examples = test.map((person, index) => (
     <ConnectionsCard name={person} key={person} />
@@ -13,7 +13,7 @@ const Connections = () => {
         <section className="">
             <div className="connections-heading">
                 <h2>Connections</h2>
-                <p>View conversations starter guide</p>
+                <p>View conversations starters</p>
             </div>
             <section className="connections">
                 {examples}
