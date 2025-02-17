d8aa4bb34b45c64b7cd65720bdc93fd320ec9b55
vishalicious213 <vishalicious213@gmail.com>
2025-02-17 14:11:52 -0800

    updated social-links form structure & added button

--- src/components/features/profile-settings/social-links.jsx
+++ src/components/features/profile-settings/social-links.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import '../../../App.css'
+import './social-links.css'
 
 const SocialLinks = () => {
     const [formData, setFormData] = useState(
@@ -23,53 +24,58 @@ const SocialLinks = () => {
     }
 
     return (
-        <>
-        <h2>Social Links</h2>
-        <form>
-            <input
-                type="text"
-                placeholder="LinkedIn"
-                onChange={handleChange}
-                name="linkedin"
-                value={formData.linkedin}
-            />
-            <input
-                type="text"
-                placeholder="Bluesky"
-                onChange={handleChange}
-                name="bluesky"
-                value={formData.bluesky}
-            />
-            <input
-                type="text"
-                placeholder="X"
-                onChange={handleChange}
-                name="twitter"
-                value={formData.twitter}
-            />
-            <input
-                type="text"
-                placeholder="GitHub"
-                onChange={handleChange}
-                name="github"
-                value={formData.github}
-            />
-            <input
-                type="text"
-                placeholder="Website"
-                onChange={handleChange}
-                name="portfolio"
-                value={formData.portfolio}
-            />
-            <input
-                type="email"
-                placeholder="Blog"
-                onChange={handleChange}
-                name="blog"
-                value={formData.blog}
-            />
-        </form>
-        </>
+        <section className="form-container">
+            <h2>Social Links</h2>
+            
+            <form className="social-links-form">
+                <input
+                    type="text"
+                    placeholder="LinkedIn"
+                    onChange={handleChange}
+                    name="linkedin"
+                    value={formData.linkedin}
+                />
+                <input
+                    type="text"
+                    placeholder="Bluesky"
+                    onChange={handleChange}
+                    name="bluesky"
+                    value={formData.bluesky}
+                />
+                <input
+                    type="text"
+                    placeholder="X"
+                    onChange={handleChange}
+                    name="twitter"
+                    value={formData.twitter}
+                />
+                <input
+                    type="text"
+                    placeholder="GitHub"
+                    onChange={handleChange}
+                    name="github"
+                    value={formData.github}
+                />
+                <input
+                    type="text"
+                    placeholder="Website"
+                    onChange={handleChange}
+                    name="portfolio"
+                    value={formData.portfolio}
+                />
+                <input
+                    type="email"
+                    placeholder="Blog"
+                    onChange={handleChange}
+                    name="blog"
+                    value={formData.blog}
+                />
+                <button>
+                    Save Links
+                    <img src="public\icons\Right.svg"></img>
+                </button>
+            </form>
+        </section>
     )
 }
 
