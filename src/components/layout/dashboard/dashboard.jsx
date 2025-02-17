58d1919300791f032c1e63951541a0fd8a464e87
vishalicious213 <vishalicious213@gmail.com>
2025-02-16 23:40:17 -0800

    drafted Connections. rendering it from Dashboard

--- src/components/layout/dashboard/dashboard.jsx
+++ src/components/layout/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Connections from './connections'
 import '../../../App.css'
 import './dashboard.css'
 
@@ -7,7 +8,9 @@ const Dashboard = () => {
         <section id="dashboard" className="dashboard">
             <h1>Home</h1>
             <section className="dashboard-containers">
-                <section id="connections-container" className="connections-container">connections</section>
+                <section id="connections-container" className="connections-container">
+                    <Connections />
+                </section>
                 <section id="chats-requests-container" className="chats-requests-container">
                     <section id="chats-container" className="chats-container">coffee chats</section>
                     <section id="requests-container" className="requests-container">requests</section>
