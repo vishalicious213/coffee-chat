a9db61091767f64a56e538a9a24d24906cf3cbf2
vishalicious213 <vishalicious213@gmail.com>
2025-02-17 00:03:48 -0800

    ConnectionsCard: added basic elements

--- src/components/layout/dashboard/connections-card.jsx
+++ src/components/layout/dashboard/connections-card.jsx
@@ -5,6 +5,9 @@ const ConnectionsCard = () => {
     return (
         <section className="connections-card">
             <img src="/avatars/avocado.jpg" alt="profile pic" />
+            <h2>Name Here <span>‧ 2d</span></h2>
+            <p>Short bio here about this person woohoo</p>
+            <button>Message</button>
         </section>
     )
 }
