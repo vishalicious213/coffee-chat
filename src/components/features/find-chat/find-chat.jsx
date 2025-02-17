988743740d6eeddb7c294637c86c6cd294a8635a
vishalicious213 <vishalicious213@gmail.com>
2025-02-17 14:26:19 -0800

    added find-chat.css

--- src/components/features/find-chat/find-chat.jsx
+++ src/components/features/find-chat/find-chat.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import "./find-chat.css"
 
 const FindChat = () => {
     return (
-        <div>Find A Chat</div>
+        <section className="find-chat">
+            <div className="find-chat-circle"></div>
+            <h2>Ready to meet someone new?</h2>
+            <p>We'll match you with another developer for a 30-minute coffee chat based on your interest and availability</p>
+            <button>Find Chat</button>
+        </section>
     )
 }
 
