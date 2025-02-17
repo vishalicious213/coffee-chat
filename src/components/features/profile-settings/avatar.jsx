fd6958a30d1b6a17ed651a5f24404e0f17e3a478
Giovanni Nazario <giovanni.nazario@gmail.com>
2025-02-17 15:20:59 -0500

    Personal Details CSS Styling v1.0

--- src/components/features/profile-settings/avatar.jsx
+++ src/components/features/profile-settings/avatar.jsx
@@ -1,51 +1,54 @@
-import React from 'react'
-import { useState } from 'react'
-import '../../../App.css'
+import React from "react";
+import { useState } from "react";
+import "../../../App.css";
 
 const avatarPics = [
-    "/avatars/avocado.jpg",
-    "/avatars/cat.jpg",
-    "/avatars/cherry.jpg",
-    "/avatars/corgi.jpg",
-    "/avatars/cupcake.jpg",
-    "/avatars/frog.jpg",
-    "/avatars/orange.jpg",
-    "/avatars/palm-tree.jpg",
-    "/avatars/pug.jpg",
-    "/avatars/tree.jpg",
-]
+  "/avatars/avocado.jpg",
+  "/avatars/cat.jpg",
+  "/avatars/cherry.jpg",
+  "/avatars/corgi.jpg",
+  "/avatars/cupcake.jpg",
+  "/avatars/frog.jpg",
+  "/avatars/orange.jpg",
+  "/avatars/palm-tree.jpg",
+  "/avatars/pug.jpg",
+  "/avatars/tree.jpg",
+];
 
 const Avatar = () => {
-    const [avatarImg, setAvatarImg] = useState(avatarPics[0])
-    const [showAvatars, setShowAvatars] = useState(false)
-    const [seeAvatars, setSeeAvatars] = useState("avatarsContainer hidden")
-    
-    const avatarPicsHtml = avatarPics.map((pic, index) => (
-        <img 
-            key={index} 
-            src={pic} 
-            alt="user avatar photo" 
-            onClick={() => setAvatarImg(pic)} 
-            className="avatar-option"
-        />
-    ))
+  const [avatarImg, setAvatarImg] = useState(avatarPics[0]);
+  const [showAvatars, setShowAvatars] = useState(false);
+  const [seeAvatars, setSeeAvatars] = useState("avatarsContainer hidden");
 
-    function handleClick() {
-        setShowAvatars(prev => !prev)
-        if (!showAvatars) {
-            setSeeAvatars("avatarsContainer avatarsContainerWhite")
-        } else {
-            setSeeAvatars("avatarsContainer hidden")
-        }
+  const avatarPicsHtml = avatarPics.map((pic, index) => (
+    <img
+      key={index}
+      src={pic}
+      alt="user avatar photo"
+      onClick={() => setAvatarImg(pic)}
+      className="avatar-option"
+    />
+  ));
+
+  function handleClick() {
+    setShowAvatars((prev) => !prev);
+    if (!showAvatars) {
+      setSeeAvatars("avatarsContainer avatarsContainerWhite");
+    } else {
+      setSeeAvatars("avatarsContainer hidden");
     }
+  }
 
-    return (
-        <section id="user-avatar" className="user-avatar">
-            <div>Avatar</div>
-            <img src={avatarImg} alt="user avatar photo" onClick={handleClick} />
-            <section id="avatarsContainer" className={seeAvatars}>{showAvatars ? avatarPicsHtml : null}</section>
-        </section>
-    )
-}
+  return (
+    <section id="user-avatar" className="user-avatar">
+      <div>Avatar</div>
+      <p className="label-text">Choose your avatar</p>
+      <img src={avatarImg} alt="user avatar photo" onClick={handleClick} />
+      <section id="avatarsContainer" className={seeAvatars}>
+        {showAvatars ? avatarPicsHtml : null}
+      </section>
+    </section>
+  );
+};
 
-export default Avatar
+export default Avatar;
