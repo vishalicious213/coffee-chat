618719d4d2e8abc00d71dd26f4a3f7b2b661ad1d
vishalicious213 <vishalicious213@gmail.com>
2025-02-16 23:36:24 -0800

    dashboard has sections for connections, coffee chats & requests

--- src/components/layout/dashboard/dashboard.jsx
+++ src/components/layout/dashboard/dashboard.jsx
@@ -5,7 +5,14 @@ import './dashboard.css'
 const Dashboard = () => {
     return (
         <section id="dashboard" className="dashboard">
-            dashboard
+            <h1>Home</h1>
+            <section className="dashboard-containers">
+                <section id="connections-container" className="connections-container">connections</section>
+                <section id="chats-requests-container" className="chats-requests-container">
+                    <section id="chats-container" className="chats-container">coffee chats</section>
+                    <section id="requests-container" className="requests-container">requests</section>
+                </section>
+            </section>
         </section>
     )
 }
