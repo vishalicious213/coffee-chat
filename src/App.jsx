ebada4c7334ca62728aab7fc10791a69a503b789
vishalicious213 <vishalicious213@gmail.com>
2025-02-16 23:11:26 -0800

    cleaned up App. removed everything that's not being used. placed rendered components into main

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import LandingPage from './components/landing-page/landing-page'
 import Navbar from './components/layout/navbar/navbar'
 import Signup from './components/features/signup/signup'
-import Dashboard from './components/layout/dashboard/dashboard'
-import ProfileSettings from './components/features/profile-settings/profile-settings'
 import './App.css'
 
 function App() {
@@ -11,22 +9,12 @@ function App() {
 
   return (
     <>
-      {/* <LandingPage changePage={setPageToLoad} /> */}
-
-      {pageToLoad === "landingpage" ? <LandingPage changePage={setPageToLoad} />
-        : pageToLoad === "navbar" ? <Navbar />
-        : pageToLoad === "signup" ? <Signup />
-        // : pageToLoad === "dashboard" ? <Dashboard />
-        : null}
-
-      {/* <Navbar /> */}
       <main>
-        {/* <ProfileSettings /> */}
+        {pageToLoad === "landingpage" ? <LandingPage changePage={setPageToLoad} />
+          : pageToLoad === "navbar" ? <Navbar />
+          : pageToLoad === "signup" ? <Signup />
+          : null}
       </main>
-      {/* <section className="app">
-        <h1>Coffee Chat Roulette</h1>
-      </section> */}
-      {/* <Dashboard /> */}
     </>
   )
 }
