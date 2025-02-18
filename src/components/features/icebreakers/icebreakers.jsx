f41bcb689b0991d91f7fe6fea4178d9464ccb688
vishalicious213 <vishalicious213@gmail.com>
2025-02-17 23:40:56 -0800

    added icebreakers.jsx & icebreakers.css

--- src/components/features/icebreakers/icebreakers.jsx
+++ src/components/features/icebreakers/icebreakers.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./icebreakers.css"
 
 const Icebreakers = () => {
     return (
-        <div>Icebreakers</div>
+        <div className="container">
+            <header>
+                <h1>Icebreaker Question Generator</h1>
+            </header>
+            
+            <main>
+                <div id="loading" className="loading">Loading questions...</div>
+                <ul id="questions-list" className="questions-list"></ul>
+                <button id="refresh-button" className="refresh-button">Get New Questions</button>
+            </main>
+            
+            <footer>
+                <p>Powered by AppWrite</p>
+            </footer>
+        </div>
     )
 }
 
