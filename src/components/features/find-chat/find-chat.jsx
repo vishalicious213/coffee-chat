d6b59133e6e6f9d79c5c99b78ae0485df5b58b57
vishalicious213 <vishalicious213@gmail.com>
2025-02-18 00:22:40 -0800

    fixing image paths to not include /public

--- src/components/features/find-chat/find-chat.jsx
+++ src/components/features/find-chat/find-chat.jsx
@@ -6,7 +6,7 @@ const FindChat = ({navTo}) => {
 
     return (
         <section className="find-chat">
-            <img src="../../../public/icons/icon-just-cup.svg" alt="" className="find-chat-circle">
+            <img src="/icons/icon-just-cup.svg" alt="" className="find-chat-circle">
             </img>
             <h2>Ready to meet someone new?</h2>
             <p>We'll match you with another developer for a 30-minute coffee chat based on your interest and availability</p>
