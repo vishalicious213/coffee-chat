3be59f35cfd2420f8823557c0d6721f8879bc5a1
vishalicious213 <vishalicious213@gmail.com>
2025-02-18 18:15:36 -0800

    added content to Match.jsx. drafted match.css

--- src/components/features/find-chat/match.jsx
+++ src/components/features/find-chat/match.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./match.css"
 
 const Match = () => {
     return (
-        <div>Match</div>
+        <section id="match" className="match">
+            <h2>Find Chat</h2>
+            <section>
+                <p>Woohoo! Check out your new match</p>
+                <p>You have a 94% compatibility score based on your interests and experience.</p>
+            </section>
+
+            <section id="render-chat-match" className="render-chat-match"></section>
+            
+            <section id="match-buttons" className="match-buttons">
+                <button id="send-chat-request-btn" className="send-chat-request-btn">Send chat request</button>
+                <button id="find-another-btn" className="find-another-btn">Find me another</button>
+            </section>
+        </section>
     )
 }
 
